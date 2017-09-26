8df3a332d7616e053bbcefd40e70841931bf8e33
zxfjd3g <258147149@qq.com>
2017-09-26 15:57:56 +0800

    init app

--- work/js/mvvm/mvvm.js
+++ work/js/mvvm/mvvm.js
@@ -1,11 +1,14 @@
 function MVVM(options) {
+    // 将配置对象保存到vm中
     this.$options = options;
+    // 将data数据对象保存到vm和data变量中
     var data = this._data = this.$options.data;
+    // 保存vm到me变量
     var me = this;
 
-    // 数据代理
-    // 实现 vm.xxx -> vm._data.xxx
-    Object.keys(data).forEach(function(key) {
+    // 遍历data中所有的属性
+    Object.keys(data).forEach(function(key) {// 属性名
+        // 对指定的属性实现数据代理
         me._proxy(key);
     });
 
@@ -20,14 +23,20 @@ MVVM.prototype = {
     },
 
     _proxy: function(key) {
+        // 保存vm
         var me = this;
+        // 给vm添加指定的属性(指定属性描述符)
         Object.defineProperty(me, key, {
-            configurable: false,
-            enumerable: true,
+            configurable: false, // 不可重新定义
+            enumerable: true, // 可以枚举
+            // 当通过vm.xxx来读取属性值时调用此方法得到属性值
             get: function proxyGetter() {
+                // data中取出对应属性的值返回
                 return me._data[key];
             },
+            // 当通过vm.xxx = value赋新值时调用
             set: function proxySetter(newVal) {
+                // 将最新的值保存data中对应的属性
                 me._data[key] = newVal;
             }
         });
